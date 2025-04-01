7ad08dcd4f05e9a11f574ce27bc64b723327ea5f
oleg <op278381@gmail.com>
2025-04-01 18:36:05 +0300

    поднетие вверх

--- js/index.js
+++ js/index.js
@@ -13,33 +13,33 @@ cnp__menu.addEventListener("click", () => {
 
 // Поднятие вверх
 
-// const btnUp = {
-//   el: document.querySelector(".up-button"),
-//   show() {
-//     // удалим у кнопки класс btn-up_hide
-//     this.el.classList.remove("button-up_hide");
-//   },
-//   hide() {
-//     // добавим к кнопке класс btn-up_hide
-//     this.el.classList.add("button-up_hide");
-//   },
-//   addEventListener() {
-//     // при прокрутке содержимого страницы
-//     window.addEventListener("scroll", () => {
-//       // определяем величину прокрутки
-//       const scrollY = window.scrollY || document.documentElement.scrollTop;
-//       // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
-//       scrollY > 500 ? this.show() : this.hide();
-//     });
-//     // при нажатии на кнопку .btn-up
-//     document.querySelector(".up-button").onclick = () => {
-//       // переместим в начало страницы
-//       window.scrollTo({
-//         top: 0,
-//         left: 0,
-//         behavior: "smooth",
-//       });
-//     };
-//   },
-// };
-// btnUp.addEventListener();
+const btnUp = {
+  el: document.querySelector(".up-button"),
+  show() {
+    // удалим у кнопки класс btn-up_hide
+    this.el.classList.remove("button-up_hide");
+  },
+  hide() {
+    // добавим к кнопке класс btn-up_hide
+    this.el.classList.add("button-up_hide");
+  },
+  addEventListener() {
+    // при прокрутке содержимого страницы
+    window.addEventListener("scroll", () => {
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+      scrollY > 500 ? this.show() : this.hide();
+    });
+    // при нажатии на кнопку .btn-up
+    document.querySelector(".up-button").onclick = () => {
+      // переместим в начало страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    };
+  },
+};
+btnUp.addEventListener();
